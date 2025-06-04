166976d862952b9663b875dcf77e95735938981f
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-04 23:37:30 +0530

    car details screen and maps page screen

--- lib/presentation/pages/map_details_screen.dart
+++ lib/presentation/pages/map_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapDetailsScreen extends StatelessWidget {
   const MapDetailsScreen({super.key});
@@ -9,6 +11,16 @@ class MapDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.red,
       ),
+      body: FlutterMap(
+          options: MapOptions(
+            initialCenter: LatLng(20.5937, 78.9629),
+            initialZoom: 13,
+
+
+
+          ),
+
+          children:Text('this happens to be the map')),
 
     );
   }
