4c5f74dd4e8279b178ca3ec15a7e821417649a26
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-05 13:23:15 +0530

    car details screen and maps page screen

---
+++ lib/data/datasources/firebase_car_data_source.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rentrover/data/models/Car.dart';
+import 'package:rentrover/domain/usecases/get_cars.dart';
+
+
+
+
+class FirebaseCarDataSource{
+  final FirebaseFirestore firestore;
+  FirebaseFirestore({required this.firestore});
+
+  Future<List<Car>> GetCars() async {
+    var snapshot =  await firestore.collection('cars').get;
+    return snapshot.docs.map((doc)=> Car.FromMap(doc.data())).toList();
+
+  }
+}
---
+++ lib/domain/usecases/get_cars.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:rentrover/data/models/Car.dart';
+import 'package:rentrover/domain/repositories/car_repository.dart';
+
+class GetCars{
+  final CarRepository repository;
+
+  GetCars(this.repository);
+
+  Future<List<Car>> call() async{
+    return await repository.fetchCars();
+  }
+
+}
