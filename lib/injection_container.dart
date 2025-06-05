5a1945549721d0bde682ba69ab33a4b928cbcaa3
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-05 15:00:32 +0530

    dependency injection

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rentrover/data/datasources/firebase_car_data_source.dart';
+import 'package:rentrover/data/repositories/car_repository_impl.dart';
+import 'package:rentrover/domain/repositories/car_repository.dart';
+import 'package:rentrover/domain/usecases/get_cars.dart';
+import 'package:rentrover/presentation/bloc/cat_bloc.dart';
 
 GetIt getIt = GetIt.instance;
 
 void initInjection(){
   try{
+    getIt.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+    getIt.registerLazySingleton<FirebaseCarDataSource>(()=>FirebaseCarDataSource(firestore: getIt<FirebaseFirestore>()));
+    getIt.registerLazySingleton<CarRepository>(()=>CarRepositoryImpl(getIt<FirebaseCarDataSource>()));
+    getIt.registerLazySingleton<GetCars>(()=>GetCars(getIt<CarRepository>()));
+    getIt.registerFactory(()=> CarBloc(getCars: getIt<GetCars>()));
+
 
   }catch(e){
     throw e;
