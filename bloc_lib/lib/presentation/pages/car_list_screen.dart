453ec9c13f55aa39c3659c46db868016d9d68bf2
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-24 01:12:44 +0530

    bloc removal

---
+++ bloc_lib/lib/presentation/pages/car_list_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:rentrover/data/models/car.dart';
+import 'package:rentrover/presentation/widgets/car_card.dart';
+
+class CarListScreen extends StatelessWidget {
+   CarListScreen({super.key});
+  final List<Car> cars=[
+    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
+    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
+    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          
+        title: Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Choose your Car",  //todo style text
+            style: TextStyle(),),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
+      body:ListView.builder(
+        itemCount: cars.length,
+          itemBuilder: (context, index) {
+            return CarCard(car: cars[index]);
+          } ,),
+    );
+  }
+}
