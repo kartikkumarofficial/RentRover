2bb30fd84180bb25131ca471393470275910390a
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-07-13 20:26:17 +0530

    Refactor: Update CarListScreen UI and greeting message
    This commit refactors the `CarListScreen` with the following changes:
    
    - The greeting message now dynamically displays the username fetched from `UserController` instead of a hardcoded name.
    - The `SafeArea` widget has been removed, allowing content to extend to the screen edges.
    - Added top padding using `SizedBox(height: Get.height*0.03,)`.
    - The `ListView.builder` has been modified to display the greeting message as part of the first item in the list, ensuring it scrolls with the content. The first car card is also included within this initial list item.

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -12,7 +12,7 @@ class AuthController extends GetxController {
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
   final isLoading = false.obs;
-  //signup function
+  //signup fun
 
   Future<void> signUp() async {
     final name = nameController.text.trim();
